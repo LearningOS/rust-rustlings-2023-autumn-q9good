e24654cbf024a26dc2c8c9e3ee62f5ffe38d5883
q9good <psnyima@gmail.com>
2023-10-10 22:26:39 +0800

    fix: crate path

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -51,7 +51,7 @@ mod my_module {
 mod tests {
     // TODO: What do we have to import to have `transformer` in scope?
     use super::Command;
-    use my_module::transformer;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
